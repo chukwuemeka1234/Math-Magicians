8c5f2e525fa620347c9937441515111c738c8902
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-01-21 01:59:40 +0100

    Refactor the calculator component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,89 +1,84 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../index.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleClick = this.handleClick.bind(this);
+const Calculator = (() => {
+  const [state, setState] = useState({});
+
+  function handleClick(e) {
+    setState((state) => calculate(state, e.target.textContent));
   }
 
-  handleClick(e) {
-    this.setState((state) => calculate(state, e.target.textContent));
-  }
+  const { next, total } = state;
 
-  render() {
-    const { next, total } = this.state;
-    return (
-      <div className="Calc-app">
-        <div className="Calc-container">
-          <div className="calc-display">
-            <span className="span-calc">{ next || total || 0 }</span>
-          </div>
-          <div className="numbers">
-            <button type="button" className="btn" onClick={this.handleClick}>
-              AC
-            </button>
-            <button type="button" className="btn" onClick={this.handleClick}>
-              +/-
-            </button>
-            <button type="button" className="btn" onClick={this.handleClick}>
-              %
-            </button>
-            <button type="button" className="btn btn-orange" onClick={this.handleClick}>
-              ÷
-            </button>
-            <button type="button" className="btn" onClick={this.handleClick}>
-              7
-            </button>
-            <button type="button" className="btn" onClick={this.handleClick}>
-              8
-            </button>
-            <button type="button" className="btn" onClick={this.handleClick}>
-              9
-            </button>
-            <button type="button" className="btn btn-orange" onClick={this.handleClick}>
-              x
-            </button>
-            <button type="button" className="btn" onClick={this.handleClick}>
-              4
-            </button>
-            <button type="button" className="btn" onClick={this.handleClick}>
-              5
-            </button>
-            <button type="button" className="btn" onClick={this.handleClick}>
-              6
-            </button>
-            <button type="button" className="btn btn-orange" onClick={this.handleClick}>
-              -
-            </button>
-            <button type="button" className="btn" onClick={this.handleClick}>
-              1
-            </button>
-            <button type="button" className="btn" onClick={this.handleClick}>
-              2
-            </button>
-            <button type="button" className="btn" onClick={this.handleClick}>
-              3
-            </button>
-            <button type="button" className="btn btn-orange" onClick={this.handleClick}>
-              +
-            </button>
-            <button type="button" className="btn zero" onClick={this.handleClick}>
-              0
-            </button>
-            <button type="button" className="btn" onClick={this.handleClick}>
-              .
-            </button>
-            <button type="button" className="btn btn-orange" onClick={this.handleClick}>
-              =
-            </button>
-          </div>
+  return (
+    <div className="Calc-app">
+      <div className="Calc-container">
+        <div className="calc-display">
+          <span className="span-calc">{ next || total || 0 }</span>
+        </div>
+        <div className="numbers">
+          <button type="button" className="btn" onClick={handleClick}>
+            AC
+          </button>
+          <button type="button" className="btn" onClick={handleClick}>
+            +/-
+          </button>
+          <button type="button" className="btn" onClick={handleClick}>
+            %
+          </button>
+          <button type="button" className="btn btn-orange" onClick={handleClick}>
+            ÷
+          </button>
+          <button type="button" className="btn" onClick={handleClick}>
+            7
+          </button>
+          <button type="button" className="btn" onClick={handleClick}>
+            8
+          </button>
+          <button type="button" className="btn" onClick={handleClick}>
+            9
+          </button>
+          <button type="button" className="btn btn-orange" onClick={handleClick}>
+            x
+          </button>
+          <button type="button" className="btn" onClick={handleClick}>
+            4
+          </button>
+          <button type="button" className="btn" onClick={handleClick}>
+            5
+          </button>
+          <button type="button" className="btn" onClick={handleClick}>
+            6
+          </button>
+          <button type="button" className="btn btn-orange" onClick={handleClick}>
+            -
+          </button>
+          <button type="button" className="btn" onClick={handleClick}>
+            1
+          </button>
+          <button type="button" className="btn" onClick={handleClick}>
+            2
+          </button>
+          <button type="button" className="btn" onClick={handleClick}>
+            3
+          </button>
+          <button type="button" className="btn btn-orange" onClick={handleClick}>
+            +
+          </button>
+          <button type="button" className="btn zero" onClick={handleClick}>
+            0
+          </button>
+          <button type="button" className="btn" onClick={handleClick}>
+            .
+          </button>
+          <button type="button" className="btn btn-orange" onClick={handleClick}>
+            =
+          </button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+});
 
 export default Calculator;
